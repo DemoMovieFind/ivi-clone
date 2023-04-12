79003a0db149cd9495df8b90c14a5c57c3951ea5
Snezh88k <snezh88k@gmail.com>
2023-04-13 01:01:49 +0300

    Storybook react-intl intergation

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,16 @@
 import type { Preview } from "@storybook/react";
 import "../src/index.css";
+import { reactIntl } from "./reactIntl";
+import { LOCALES } from "../src/i18n/locales";
 
 const preview: Preview = {
+  globals: {
+    locale: reactIntl.defaultLocale,
+    locales: {
+      [LOCALES.ENGLISH]: "English",
+      [LOCALES.RUSSIAN]: "Russian",
+    },
+  },
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -10,6 +19,7 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    reactIntl,
   },
 };
 
