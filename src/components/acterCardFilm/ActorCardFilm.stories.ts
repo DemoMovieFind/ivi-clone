6702e73ae3c08895ef4c005ba47762d1a7b02d6e
Snezh88k <snezh88k@gmail.com>
2023-04-14 16:27:28 +0300

    рефакторинг элемента карточки актера

---
+++ src/components/acterCardFilm/ActorCardFilm.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ActorCardFilm } from "./ActorCardFilm";
+
+const meta = {
+  title: "ActorCardFilm/ActorCardFilm",
+  component: ActorCardFilm,
+  tags: ["autodocs"],
+} satisfies Meta<typeof ActorCardFilm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    appearance: "default",
+    film: {
+      id: 498500,
+      title: "Двойная петля",
+      posters: {
+        small: {
+          url: "https://thumbs.dfs.ivi.ru/storage37/contents/2/2/29f0af23c55d95f2c205549d25feaa.jpg/234x360/?q=85",
+        },
+      },
+      rating: {
+        ivi: 3.7,
+      },
+      year: 2022,
+    },
+  },
+};
---
+++ src/components/acterCardFilm/ActorCardFilm.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./ActorCardFilm.module.css";
+import clsx from "clsx";
+import { Info } from "./info/Info";
+import { ButtonLink } from "../buttons/ButtonLink";
+import { Film } from "../../types/entities/Film";
+
+export interface ActorCardFilmProps {
+  appearance?: "default";
+  className?: string;
+  film: Film;
+}
+
+export const ActorCardFilm = ({
+  appearance = "default",
+  className,
+  film,
+  ...props
+}: ActorCardFilmProps) => {
+  const href = `https://www.ivi.ru/watch/${film.id}`;
+  return (
+    <div
+      className={clsx(styles.card, styles[appearance], className)}
+      {...props}
+    >
+      <img
+        src={film?.posters?.small?.url}
+        alt="img_film"
+        className={styles.filmImage}
+      />
+      <div className={styles.main}>
+        <Info year={film.year} title={film.title} rating={film.rating?.ivi} />
+        <ButtonLink href={href} size="large">
+          Подробнее
+        </ButtonLink>
+      </div>
+    </div>
+  );
+};
