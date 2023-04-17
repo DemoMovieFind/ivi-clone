b3b1d496c16328b47e13ade5aae4ce5cff2d626b
zoomkaz <zoomka.rus@mail.ru>
2023-04-17 16:38:28 +0300

    Добавлена карточка актёров "ещё"

---
+++ src/components/person/personMoreCard/PersonMoreCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './PersonMoreCard.module.css'
+
+const PersonMoreCard = () => {
+  return (
+    <div className={styles.personMoreContainer}>
+      <div className={styles.moreText}>Ещё</div>
+    </div>
+  )
+}
+
+export default PersonMoreCard
