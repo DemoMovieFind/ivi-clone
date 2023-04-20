0e556b293c7fc975f9a0dcf76dcb61f94a68544e
Vadim Manushin <vadimmanushin@yandex.ru>
2023-04-20 15:52:12 +0300

    fix nav list

--- src/components/navList/NavList.stories.tsx
+++ src/components/navList/NavList.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
 import NavList from "./NavList";
+import { BrowserRouter } from "react-router-dom";
 
 const meta: Meta<typeof NavList> = {
   title: "UI Components/NavList",
@@ -9,5 +10,15 @@ const meta: Meta<typeof NavList> = {
 
 type Story = StoryObj<typeof meta>;
 
-export const navList: Story = {};
+export const navList: Story = {
+  render: () => <BrowserRouter>
+    <NavList 
+      links={
+        [{ href:'link1', translationId:'nav_list_films' },
+        { href:'link2', translationId:'nav_list_series' },
+        { href:'link3', translationId:'nav_list_certificate_activation', marked:true },]
+      }
+    vertical={false} 
+    /></BrowserRouter>,
+};
 export default meta;
