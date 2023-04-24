946ece0b34e1a7857a7b499f8cc35ab8fc4ff18d
Snezh88k <snezh88k@gmail.com>
2023-04-24 18:23:53 +0300

    исправил конфликты

--- src/index.tsx
+++ src/index.tsx
@@ -3,36 +3,29 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import "./index.css";
-import { IntlProvider } from "react-intl";
-import { LOCALES } from "./i18n/locales";
-import { messages } from "./i18n/messages";
-import FilmsPage from "./pages/FilmsPage";
 import { store } from "./store/store";
 import AuthPage from "./pages/AuthPage";
+import "./index.css";
 import Template from "./pages/Template";
+import FilmsPage from "./pages/FilmsPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-const browserLocale = navigator.language;
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <IntlProvider
-        messages={messages[browserLocale]}
-        locale={browserLocale}
-        defaultLocale={LOCALES.RUSSIAN}
-      >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Template content={<MainPage />} />} />
-            <Route path="/movies" element={<FilmsPage />} />
-            <Route path="/auth" element={<Template content={<AuthPage />} />} />
-          </Routes>
-        </BrowserRouter>
-      </IntlProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Template content={<MainPage />} />} />
+          <Route path="/auth" element={<Template content={<AuthPage />} />} />
+          <Route
+            path="/movies"
+            element={<Template content={<FilmsPage />} />}
+          />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
