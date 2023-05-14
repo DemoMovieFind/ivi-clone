d74279e1ee06ffa856e67ca5be8f6efa183aa4fa
Snezh88k <snezh88k@gmail.com>
2023-05-14 11:18:17 +0300

    ребилд авторизации вк

--- src/authVK.tsx
+++ src/authVK.tsx
@@ -1,27 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function LoginVKPage() {
-  const auth = () => {
-    const code = document.location.search;
-    fetch(
-      `https://oauth.vk.com/access_token?client_id=51640846&client_secret=qBziVEz4TLY99LYBDfRf&redirect_uri==http://localhost:3000&code=${code}`,
-      {
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
-      .then((response) => response.ok)
-      .then((data) => console.log(data));
-  };
+  const pathname = document.location.pathname;
+
+  const [currentUrl, setCurrentUrl] = React.useState(pathname);
+
+  useEffect(() => {
+    console.log(document.location.hash);
+  }, [currentUrl]);
   return (
     <div>
       LoginVKPage
-      <a href="https://oauth.vk.com/authorize?client_id=51640846&display=page&redirect_uri=http://localhost:3000&scope=friends&response_type=code&v=5.131">
+      <a href="https://oauth.vk.com/authorize?client_id=51640846&display=page&redirect_uri=http://localhost:3000&scope=friends&response_type=token&v=5.131">
         <div>Авторизация</div>
       </a>
-      <button onClick={auth}>Получить Токен</button>
     </div>
   );
 }
