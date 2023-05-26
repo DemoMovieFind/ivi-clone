059dfea532e3ae078736c9d2074276cd457c925b
Vadim Manushin <vadimmanushin@yandex.ru>
2023-05-26 17:20:32 +0300

    fixed story of authform

--- src/components/AuthForm/AuthForm.stories.tsx
+++ src/components/AuthForm/AuthForm.stories.tsx
@@ -1,12 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Meta, StoryObj } from '@storybook/react';
-
 import AuthForm, { OutputAuthForm } from './AuthForm';
-import ComponentWrapper from '../../../tests/ComponentWrapper';
 import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import RouterWrapper from '../../../.storybook/routerWrapper';
 
 const meta = {
   title: "Forms/AuthForm",
@@ -16,21 +15,20 @@ const meta = {
 
 export default meta;
 
-export const Default: Story = {
-  render:(args)=> {return <Provider store={store}>
-      <GoogleOAuthProvider 
+export const authForm: Story = {
+  args: {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    handleSubmit: (data: OutputAuthForm)=>{},
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    handleGoogle:()=>{}
+  },
+  render:(args) => <Provider store={store}>
+      <GoogleOAuthProvider
         clientId="64086974939-oijgmdetcv1c9a6envjks8qoov02adgp.apps.googleusercontent.com">
-        <MemoryRouter>
-          <AuthForm handleGoogle={args.handleGoogle} handleSubmit={args.handleSubmit}/>
-        </MemoryRouter>
+          {RouterWrapper(<AuthForm  {...args} />)}
       </GoogleOAuthProvider>
-    </Provider>},
-  // args: {
-  //   handleSubmit: (data: OutputAuthForm)=>{},
-  //   handleGoogle:()=>void
-  // },
+    </Provider>,
 };
 
 type Story = StoryObj<typeof meta>;
 
-export const authForm: Story = {}
